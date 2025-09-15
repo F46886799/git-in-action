1abc9a5b8ef3d20a587994928163181a47979672
Frank <f.46886799@gmail.com>
2025-09-15 10:39:37 +0800

    Update hi5_us_script.py

--- hi5_us_script.py
+++ hi5_us_script.py
@@ -2,9 +2,8 @@ from utils import *
 import time
 
 if __name__ == "__main__":
-    
     logger.info("美股Hi5跟踪开始")
-    content_txt = "{}，请查看：{}".format(today,url)
+    content_txt = "{}，美股Hi5跟踪：{}".format(get_current_date(),"https://docs.google.com/spreadsheets/d/1G1E0qtLzt1WulfUk2uSxXrm_HNKejTMwwI-4KF3OG9w/edit?gid=0#gid=0")
     bark_push(title="美股Hi5跟踪",content=content_txt)
     time.sleep(1)
     pushdeer_push(title="美股Hi5跟踪",content=content_txt)
