1b4d2576e384ba7ce8fbdf3028d0c2f94a68ff60
Frank <f.46886799@gmail.com>
2025-10-22 13:44:44 +0800

    Update hi5_cn_script.py

--- hi5_cn_script.py
+++ hi5_cn_script.py
@@ -2,14 +2,17 @@ from utils import *
 import time
 
 if __name__ == "__main__":
-    logger.info("A股Hi5跟踪开始")
-    content_txt = "{}，A股Hi5跟踪：{}".format(get_current_date(),"https://docs.google.com/spreadsheets/d/1G1E0qtLzt1WulfUk2uSxXrm_HNKejTMwwI-4KF3OG9w/edit?gid=0#gid=0")
+
+    logger.info("全天候组合A股Hi5开始")
+    content_txt = "{}，交易跟踪：{}，再平衡：{}。".format(get_current_date(),
+                                              "https://docs.google.com/spreadsheets/d/19dkG30h9jeVHrF5O_ugqwflmfdUItm6FSMccM_igRnc/edit?gid=0#gid=0",
+                                              "https://docs.google.com/spreadsheets/d/16WjXzYSZgrJNy6YgNgwJvB3JdLhhmbe-A9rlohLWmwg/edit?gid=1843166815#gid=1843166815")
     
     logger.info("***Step1:Bark推送,{}".format(get_current_time()))
-    bark_push(title="A股Hi5跟踪",content=content_txt)
+    bark_push(title="全天候组合A股Hi5",content=content_txt)
     
     time.sleep(1)
     
     logger.info("***Step2:Pushdeer推送,{}".format(get_current_time()))
-    pushdeer_push(title="A股Hi5跟踪",content=content_txt)
-    logger.info("A股Hi5跟踪结束")
+    pushdeer_push(title="全天候组合A股Hi5",content=content_txt)
+    logger.info("全天候组合A股Hi5结束")
