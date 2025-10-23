ea4fd86e83d3d53dd16262541f038ad0037bdaff
Frank <f.46886799@gmail.com>
2025-10-23 21:36:31 +0800

    Update hi5_cn_script.py

--- hi5_cn_script.py
+++ hi5_cn_script.py
@@ -3,16 +3,16 @@ import time
 
 if __name__ == "__main__":
 
-    logger.info("全天候组合A股Hi5开始")
+    logger.info("【全天候组合A股】【张甫的国君普通】开始")
     content_txt = "{}，交易跟踪：{}，再平衡：{}。".format(get_current_date(),
                                               "https://docs.google.com/spreadsheets/d/19dkG30h9jeVHrF5O_ugqwflmfdUItm6FSMccM_igRnc/edit?gid=0#gid=0",
                                               "https://docs.google.com/spreadsheets/d/16WjXzYSZgrJNy6YgNgwJvB3JdLhhmbe-A9rlohLWmwg/edit?gid=1843166815#gid=1843166815")
     
     logger.info("***Step1:Bark推送,{}".format(get_current_time()))
-    bark_push(title="全天候组合A股Hi5",content=content_txt)
+    bark_push(title="【全天候组合A股】【张甫的国君普通】",content=content_txt)
     
     time.sleep(1)
     
     logger.info("***Step2:Pushdeer推送,{}".format(get_current_time()))
-    pushdeer_push(title="全天候组合A股Hi5",content=content_txt)
-    logger.info("全天候组合A股Hi5结束")
+    pushdeer_push(title="【全天候组合A股】【张甫的国君普通】",content=content_txt)
+    logger.info("【全天候组合A股】【张甫的国君普通】结束")
