4b06b8f01afff5551601c0cf3317038d10c498ad
Frank <f.46886799@gmail.com>
2025-10-23 21:38:30 +0800

    Update logging and push notifications for portfolio

--- permanent_portfolio_script.py
+++ permanent_portfolio_script.py
@@ -3,16 +3,16 @@ import time
 
 if __name__ == "__main__":
 
-    logger.info("永久组合开始")
+    logger.info("【永久组合全球】【王慧玲的汇丰中国】开始")
     content_txt = "{}，交易跟踪：{}，再平衡：{}。".format(get_current_date(),
                                               "https://docs.google.com/spreadsheets/d/181D3VALuL9P3yHlh8tKZDSdH6fuMqUpTE0B5x6NFakM/edit?gid=0#gid=0",
                                               "https://docs.google.com/spreadsheets/d/1Jk7GYXWLAHBjsXZtUMuBtmpE3EWdIDviGOPWKb8hnJs/edit?gid=1843166815#gid=1843166815")
     
     logger.info("***Step1:Bark推送,{}".format(get_current_time()))
-    bark_push(title="永久组合",content=content_txt)
+    bark_push(title="【永久组合全球】【王慧玲的汇丰中国】",content=content_txt)
     
     time.sleep(1)
     
     logger.info("***Step2:Pushdeer推送,{}".format(get_current_time()))
-    pushdeer_push(title="永久组合",content=content_txt)
-    logger.info("永久组合结束")
+    pushdeer_push(title="【永久组合全球】【王慧玲的汇丰中国】",content=content_txt)
+    logger.info("【永久组合全球】【王慧玲的汇丰中国】结束")
