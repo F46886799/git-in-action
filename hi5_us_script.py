59fceab2370eb845a6a9928be95e22cc16753320
Frank <f.46886799@gmail.com>
2025-10-23 21:37:26 +0800

    Update log messages and push titles for Hi5

--- hi5_us_script.py
+++ hi5_us_script.py
@@ -2,14 +2,14 @@ from utils import *
 import time
 
 if __name__ == "__main__":
-    logger.info("全天候组合美股Hi5开始")
+    logger.info("【全天候组合美股】【张甫的华泰国际】开始")
     content_txt = "{}，交易跟踪：{}；再平衡：{}。".format(get_current_date(),"https://docs.google.com/spreadsheets/d/1uWR55sm4PoAZSG6KRh4T2guF91bdPaVcEQfis2p_tY8/edit?gid=0#gid=0","https://docs.google.com/spreadsheets/d/1g96AXTEuqP0uZbBhH6HkK1ayqK5nh2srQvgC4Gu6E4I/edit?gid=1843166815#gid=1843166815")
     
     logger.info("***Step1:Bark推送,{}".format(get_current_time()))
-    bark_push(title="全天候组合美股Hi5",content=content_txt)
+    bark_push(title="【全天候组合美股】【张甫的华泰国际】",content=content_txt)
     
     time.sleep(1)
     
     logger.info("***Step2:Pushdeer推送,{}".format(get_current_time()))
-    pushdeer_push(title="全天候组合美股Hi5",content=content_txt)
-    logger.info("全天候组合美股Hi5结束")
+    pushdeer_push(title="【全天候组合美股】【张甫的华泰国际】",content=content_txt)
+    logger.info("【全天候组合美股】【张甫的华泰国际】结束")
