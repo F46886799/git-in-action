bc55a9c90bd45d133a7c806362f6ca70cd46abc3
Frank <f.46886799@gmail.com>
2025-10-26 09:52:18 +0800

    Update hi5_us_script.py

--- hi5_us_script.py
+++ hi5_us_script.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     content_txt = "{}，交易跟踪：{}；再平衡：{}。".format(get_current_date(),"https://docs.google.com/spreadsheets/d/1uWR55sm4PoAZSG6KRh4T2guF91bdPaVcEQfis2p_tY8/edit?gid=0#gid=0","https://docs.google.com/spreadsheets/d/1g96AXTEuqP0uZbBhH6HkK1ayqK5nh2srQvgC4Gu6E4I/edit?gid=1843166815#gid=1843166815")
     
     logger.info("***Step1:Bark推送,{}".format(get_current_time()))
-    bark_push(title="【全天候组合美股】【张甫的华泰国际】",content=content_txt)
+    bark_push(title="【全天候组合美股】【张甫的华泰国际】",content=content_txt,category="全天候组合美股")
     
     time.sleep(1)
     
