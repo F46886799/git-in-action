0f60388ade097e65ac1ffcaf1dd211681501a9e7
Frank <f.46886799@gmail.com>
2025-10-31 17:51:10 +0800

    Update hi5_us_script.py

--- hi5_us_script.py
+++ hi5_us_script.py
@@ -3,7 +3,13 @@ import time
 
 if __name__ == "__main__":
     logger.info("【全天候组合美股】【张甫的华泰国际】开始")
-    content_txt = "{}，交易跟踪：{}；再平衡：{}。".format(get_current_date(),"https://docs.google.com/spreadsheets/d/1uWR55sm4PoAZSG6KRh4T2guF91bdPaVcEQfis2p_tY8/edit?gid=0#gid=0","https://docs.google.com/spreadsheets/d/1g96AXTEuqP0uZbBhH6HkK1ayqK5nh2srQvgC4Gu6E4I/edit?gid=1843166815#gid=1843166815")
+
+    crashed = target_crash(ticker="RSP.US")
+
+    if crashed:
+        content_txt = "{}，跟踪ETF（RSP.US）的跌幅已超过1%，需要开仓。交易跟踪：{}；再平衡：{}。".format(get_current_date(),"https://docs.google.com/spreadsheets/d/1uWR55sm4PoAZSG6KRh4T2guF91bdPaVcEQfis2p_tY8/edit?gid=0#gid=0","https://docs.google.com/spreadsheets/d/1g96AXTEuqP0uZbBhH6HkK1ayqK5nh2srQvgC4Gu6E4I/edit?gid=1843166815#gid=1843166815")
+    else:
+        content_txt = "{}，跟踪ETF（RSP.US）的状态正常，无需开仓。交易跟踪：{}；再平衡：{}。".format(get_current_date(),"https://docs.google.com/spreadsheets/d/1uWR55sm4PoAZSG6KRh4T2guF91bdPaVcEQfis2p_tY8/edit?gid=0#gid=0","https://docs.google.com/spreadsheets/d/1g96AXTEuqP0uZbBhH6HkK1ayqK5nh2srQvgC4Gu6E4I/edit?gid=1843166815#gid=1843166815")
     
     logger.info("***Step1:Bark推送,{}".format(get_current_time()))
     bark_push(title="【全天候组合美股】【张甫的华泰国际】",content=content_txt,category="全天候组合美股")
