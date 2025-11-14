01f98d1f0b9c32cf4c8932bf4c9ad1f004924f7c
Frank <f.46886799@gmail.com>
2025-11-14 13:55:19 +0800

    Add bark_push_v2 function for Bark notifications
    Added a new function 'bark_push_v2' for sending notifications via the Bark API with enhanced parameters.

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import requests
 from loguru import logger
 from BarkNotificator import BarkNotificator
 from pypushdeer import PushDeer
-
+import json
 
 def target_crash(ticker="RSP.US"):
     response = requests.get("https://eodhd.com/api/real-time/{}?api_token=69042d75473ad0.16653704&fmt=json".format(ticker))
@@ -23,26 +23,47 @@ def target_crash(ticker="RSP.US"):
         logger.debug("RSP.US的跌幅小于1%")
         return False
 
-
 def bark_push(title="welcome", content="hello world", category=None):
     bark = BarkNotificator(device_token="b4940a7bea9fa620ff521d9c31162125f0e0e07601b97cb5af6ac6de453511d7")
     bark.send(title=title, content=content,category=category)
 
+def bark_push_v2(title = "bleem", body = "Test Bark Server",
+                 group = "test", url = "https://mritd.com",
+                 icon = "https://00.ifreesite.com/world-i/united_states_of_america_flag.png"):
+    try:
+        response = requests.post(
+            url="https://api.day.app/2LPpyPuaMQibh6bEMNWQNd",
+            headers={
+                "Content-Type": "application/json; charset=utf-8",
+            },
+            data=json.dumps({
+                "title": title,
+                "body": body,
+                "sound": "bell.caf",
+                "badge": 1,
+                "icon": icon,
+                "group": group,
+                "url": url
+            })
+        )
+        print('Response HTTP Status Code: {status_code}'.format(
+            status_code=response.status_code))
+        print('Response HTTP Response Body: {content}'.format(
+            content=response.content))
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
 
 def pushdeer_push(title="# hello world", content="**optional** description in markdown"):
     pushdeer = PushDeer(pushkey="PDU21717TuwqG9EyGgLXfSEmNGcpQbj0gSqWEXvL8")
     pushdeer.send_markdown(title, desp=content)
 
-
 def get_current_time(tz: str = "Asia/Shanghai", time_format: str = "%Y-%m-%d %H:%M:%S"):
     current_time = dt.now(timezone(tz)).strftime(time_format)
     return current_time
 
-
 def get_current_date(tz: str = "Asia/Shanghai", date_format: str = "%Y-%m-%d"):
     current_date = dt.now(timezone(tz)).strftime(date_format)
     return current_date
-
 
 def requests_get(url: str, headers=None, params=None):
     # 发送 GET 请求，连接超时30秒，响应超时60*60*4秒，即4个小时
@@ -57,7 +78,6 @@ def requests_get(url: str, headers=None, params=None):
         logger.info(response.json())  # 将响应内容解析为 JSON
     else:
         logger.info("请求失败:", response.status_code)
-
 
 def requests_post(url: str, headers=None, data=None):
     # 发送 POST 请求，连接超时30秒，响应超时60*60*4秒，即4个小时
