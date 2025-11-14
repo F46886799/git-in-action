3cbee5d9b725318bb0b67dda590962eaab0a8a15
Frank <f.46886799@gmail.com>
2025-11-14 14:27:04 +0800

    Update permanent_portfolio_script.py

--- permanent_portfolio_script.py
+++ permanent_portfolio_script.py
@@ -9,7 +9,10 @@ if __name__ == "__main__":
                                               "https://docs.google.com/spreadsheets/d/1Jk7GYXWLAHBjsXZtUMuBtmpE3EWdIDviGOPWKb8hnJs/edit?gid=1843166815#gid=1843166815")
     
     logger.info("***Step1:Bark推送,{}".format(get_current_time()))
-    bark_push(title="【永久组合全球】【王慧玲的汇丰中国】",content=content_txt,category="永久组合全球")
+    # bark_push(title="【永久组合全球】【王慧玲的汇丰中国】",content=content_txt,category="永久组合全球")
+    bark_push_v2(title="【永久组合全球】【王慧玲的汇丰中国】",body=content_txt, group="永久组合全球",
+                 icon="https://00.ifreesite.com/world-i/hong_kong_national_emblem.png",
+                 url="https://docs.google.com/spreadsheets/d/1G1E0qtLzt1WulfUk2uSxXrm_HNKejTMwwI-4KF3OG9w/edit?gid=0#gid=0")
     
     time.sleep(1)
     
