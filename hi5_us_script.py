7d1f15dda422d1e7c3faf70620053e9dc991d760
Frank <f.46886799@gmail.com>
2025-11-14 13:56:02 +0800

    Replace bark_push with bark_push_v2 function

--- hi5_us_script.py
+++ hi5_us_script.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         content_txt = "{}，跟踪ETF（RSP.US）的状态【正常，无需开仓】。交易跟踪：{}；再平衡：{}；原始跟踪：{}。".format(get_current_date(),"https://docs.google.com/spreadsheets/d/1uWR55sm4PoAZSG6KRh4T2guF91bdPaVcEQfis2p_tY8/edit?gid=0#gid=0","https://docs.google.com/spreadsheets/d/1g96AXTEuqP0uZbBhH6HkK1ayqK5nh2srQvgC4Gu6E4I/edit?gid=1843166815#gid=1843166815","https://docs.google.com/spreadsheets/d/1G1E0qtLzt1WulfUk2uSxXrm_HNKejTMwwI-4KF3OG9w/edit?gid=0#gid=0")
     
     logger.info("***Step1:Bark推送,{}".format(get_current_time()))
-    bark_push(title="【全天候组合美股】【张甫的华泰国际】",content=content_txt,category="全天候组合美股")
+    bark_push_v2(title="【全天候组合美股】【张甫的华泰国际】",body=content_txt, category="全天候组合美股")
     
     time.sleep(1)
     
