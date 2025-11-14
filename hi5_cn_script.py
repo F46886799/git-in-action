8574f3e8eedb03c4895966c239a5176f5f0fba2a
Frank <f.46886799@gmail.com>
2025-11-14 14:01:46 +0800

    Update hi5_cn_script.py

--- hi5_cn_script.py
+++ hi5_cn_script.py
@@ -9,7 +9,10 @@ if __name__ == "__main__":
                                               "https://docs.google.com/spreadsheets/d/16WjXzYSZgrJNy6YgNgwJvB3JdLhhmbe-A9rlohLWmwg/edit?gid=1843166815#gid=1843166815")
     
     logger.info("***Step1:Bark推送,{}".format(get_current_time()))
-    bark_push(title="【全天候组合A股】【张甫的国君普通】",content=content_txt,category="全天候组合A股")
+    # bark_push(title="【全天候组合A股】【张甫的国君普通】",content=content_txt,category="全天候组合A股")
+    bark_push_v2(title="【全天候组合A股】【张甫的国君普通】",body=content_txt, group="全天候组合A股",
+                 icon="https://00.ifreesite.com/world-i/china_flag.png",
+                 url="https://docs.google.com/spreadsheets/d/1G1E0qtLzt1WulfUk2uSxXrm_HNKejTMwwI-4KF3OG9w/edit?gid=0#gid=0")
     
     time.sleep(1)
     
